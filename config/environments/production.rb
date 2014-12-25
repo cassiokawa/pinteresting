ab1a98ff658c21991419ae8eedd44c43b888b4d0
Cassio Kawakita <cassio.kawakita@gmail.com>
2014-12-25 19:21:29 -0200

    INstalled devise and create user signup and signin session

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  # Required by Heroku
+  # Note set this to your actual host name
+  config.action_mailer.default_url_options = { host: => 'sanpresent.com' }
+
 end
